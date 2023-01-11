da17762c554c03f9ab40f28ae44f58a4b1f562e1
Johnpaul1607 <harryninja1607@gmail.com>
2023-01-12 00:08:30 +0530

    new changes occur

--- Aruviapp/urls.py
+++ Aruviapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('course/',views.course,name='course'),
     path('gallery/',views.gallery,name='gallery'),
     path('about/',views.about,name='about'),
